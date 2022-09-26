af6de491736d02ae3c6d499f579ef0af0859e56a
Ending2015a <zexlus1126@gapp.nthu.edu.tw>
2022-09-27 00:32:25 +0800

    update shadow v2

---
+++ kage-BRP/Assets/Scripts/Rotate.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    [Range(-200.0f, 200.0f)]
+    public float speed = 10.0f;
+    public Transform center;
+
+    public Transform lookAt;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.RotateAround(center.position, Vector3.up, speed * Time.deltaTime);
+
+        if (lookAt != null)
+        {
+            transform.LookAt(lookAt, Vector3.up);
+        }
+    }
+}
---
+++ kage-BRP/Assets/Scripts/Switch.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Switch : MonoBehaviour
+{
+
+    public float period = 2.0f;
+
+    public Material lightOnMat;
+    public Material lightOffMat;
+
+    public Light light;
+
+    public bool lightOn = true;
+
+    private float curTime = 0.0f;
+
+    void SwitchLight()
+    {
+        if (light != null)
+            light.enabled = lightOn;
+        if (lightOn)
+        {
+            GetComponent<Renderer>().material = lightOnMat;
+        }
+        else
+        {
+            GetComponent<Renderer>().material = lightOffMat;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SwitchLight();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        curTime += Time.deltaTime;
+        if (curTime > period)
+        {
+            curTime = 0f;
+            lightOn = !lightOn;
+            SwitchLight();
+        }
+    }
+}
